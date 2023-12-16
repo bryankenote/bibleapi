610e9b16e627a4efbca138f11117c00054e49e25
bryankenote <brykenote@gmail.com>
2023-12-16 12:31:58 -0800

    simple api

---
+++ src/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"flag"
+
+	"log"
+	"net/http"
+
+	biblev1 "bibleapi/src/codegen/pb/bible/v1"
+	"bibleapi/src/codegen/pb/bible/v1/biblev1connect"
+	"bibleapi/src/mappers"
+
+	"bibleapi/src/db"
+
+	"connectrpc.com/connect"
+	"github.com/joho/godotenv"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+type BibleServer struct{}
+
+func (s *BibleServer) GetChapter(
+	ctx context.Context,
+	req *connect.Request[biblev1.GetChapterRequest],
+) (*connect.Response[biblev1.GetChapterResponse], error) {
+	log.Println("Request headers: ", req.Header())
+
+	verses, err := db.Instance.GetChapter(ctx, *mappers.FromGetChapterRequest(req))
+	if err != nil {
+		log.Fatalf("Unable to load verse: %s", err.Error())
+	}
+
+	res := connect.NewResponse(&biblev1.GetChapterResponse{
+		Verses: mappers.ToVerseDtos(verses),
+	})
+	res.Header().Set("Bible-Version", "v1")
+	return res, nil
+}
+
+func init() {
+	flag.Parse()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Unable to load .env file")
+	}
+
+	db.ConnectToDB()
+}
+
+func main() {
+	bibleServer := &BibleServer{}
+	mux := http.NewServeMux()
+	path, handler := biblev1connect.NewBibleServiceHandler(bibleServer)
+	mux.Handle(path, handler)
+	http.ListenAndServe("localhost:8000", h2c.NewHandler(mux, &http2.Server{}))
+}
---
+++ src/mappers/verse.go
@@ -0,0 +1,30 @@
+package mappers
+
+import (
+	biblev1 "bibleapi/src/codegen/pb/bible/v1"
+	"bibleapi/src/codegen/sqlc"
+
+	"connectrpc.com/connect"
+)
+
+func FromGetChapterRequest(req *connect.Request[biblev1.GetChapterRequest]) *sqlc.GetChapterParams {
+	return &sqlc.GetChapterParams{
+		Translation: req.Msg.Translation,
+		Book:        req.Msg.Book,
+		Chapter:     int64(req.Msg.Chapter),
+	}
+}
+
+func ToVerseDtos(verses []sqlc.Verse) []*biblev1.Verse {
+	var versesDto []*biblev1.Verse
+	for _, verse := range verses {
+		versesDto = append(versesDto, &biblev1.Verse{
+			Translation: verse.Translation,
+			Book:        verse.Book,
+			Chapter:     int32(verse.Chapter),
+			Verse:       int32(verse.Verse),
+			Content:     verse.Content,
+		})
+	}
+	return versesDto
+}
